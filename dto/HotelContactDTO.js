2f001dea58f5cd644fff7b1793dc5d7956c4e7bf
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-11-06 17:13:35 +0530

    Updated Front end

--- dto/HotelContactDTO.js
+++
@@ -1,6 +0,0 @@
-export class HotelContactDTO{
-    constructor(hotelContact_1,hotelContact_2) {
-        this.hotelContact_1=hotelContact_1;
-        this.hotelContact_2=hotelContact_2;
-    }
-}
--- dto/HotelRoomDTO.js
+++
@@ -1,9 +0,0 @@
-export class HotelRoomDTO{
-    constructor(hotelRoom_Opt_1,hotelRoom_Opt_2,
-                hotelRoom_Opt_3,hotelRoom_Opt_4) {
-        this.hotelRoom_Opt_1=hotelRoom_Opt_1;
-        this.hotelRoom_Opt_2=hotelRoom_Opt_2;
-        this.hotelRoom_Opt_3=hotelRoom_Opt_3;
-        this.hotelRoom_Opt_4=hotelRoom_Opt_4;
-    }
-}
